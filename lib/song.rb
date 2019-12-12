1bb9bbda66bd60662e19cde7110240325e28859c
Luis Herrera <herrera.lrh@gmail.com>
2019-12-12 01:23:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,8 +31,12 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      genre_count[genre]
-      binding.pry
+      if genre_count[genre] 
+        genre_count += 1
+      else
+        genre_count[genre] = 1
+      end
     end
+    genre_count
   end
 end
