4f6909fb51ec334fe0a38dd674ba37f83c5ddaab
Luis Herrera <herrera.lrh@gmail.com>
2019-12-12 01:07:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,55 +1,6 @@
 require 'pry'
 
 class Song
-  attr_accessor :name, :artist, :genre
-
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@count += 1
-    @@genres << genre
-    @@artists << artist
-  end
-
-  @@count = 0
-  @@genres = []
-  @@artists = []
-
-  def self.count
-    @@count
-  end
-
-  def self.genres
-    @@genres.uniq
-  end
-
-  def self.artists
-    @@artists.uniq
-  end
-
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
-  end
-
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |a|
-      if artist_count[a]
-        artist_count[a] += 1
-      else
-        artist_count[a] = 1
-      end
-    end
-    artist_count
-  end
+  
   
 end
